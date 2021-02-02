6817819ecc5ee08656759cb529260d96e7d4462a
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-02-02 23:25:23 +0900

    .

--- es6/arrowfunction.js
+++ es6/arrowfunction.js
@@ -16,4 +16,25 @@ function not1(x){
 
 const not2 = x => !x;
 
-var relationship1
+var relationship1 = {
+    name : 'jiwoong',
+    friends : ['jiwoong1', 'jiwoong2', 'jiwoong3'],
+    logFriends : function(){
+        var that = this;
+        this.friends.forEach(function (friend){
+            console.log(that.name, friend);
+        });
+    },
+};
+relationship1.logFriends();
+
+const relationship2 = {
+    name : 'jiwoong',
+    friends : ['jiwoong1', 'jiwoong2', 'jiwoong3'],
+    logFriends(){
+        this.friends.forEach(friend => {
+            console.log(this.name, friend);
+        });
+    },
+};
+relationship2.logFriends();
