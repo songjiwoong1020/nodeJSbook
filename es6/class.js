cd3026b44db6acd88dd446cc852375978139ccb6
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-02-06 22:10:11 +0900

    .

--- es6/class.js
+++ es6/class.js
@@ -1,3 +1,26 @@
 var Human = function(type){
     this.type = type || 'human';
 };
+
+Human.isHuman = function(human){
+    return human instanceof Human;
+}
+
+Human.prototype.breathe = function(){
+    alert(['h-a-a-a-m']);
+};
+
+var zero = function(type, firstName, lastName){
+    Human.apply(this, arguments);
+    this.firstName = firstName;
+    this.lastName = lastName;
+};
+
+zero.prototype = Object.create(Human.prototype);
+zero.prototype.constructor = Zero;
+Zero.prototype.sayName = function(){
+    alert(this.firstName + '' + this.lastName);
+};
+
+var oldZero = new Zero('human', 'Zero', 'Cho');
+Human.isHuman(oldZero);
