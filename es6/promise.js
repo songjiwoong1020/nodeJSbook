8cde67dd18e9fc25908eb022017c59fb0a14a6d0
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-02-09 22:30:05 +0900

    .

--- es6/promise.js
+++ es6/promise.js
@@ -6,3 +6,14 @@ const promise = new Promise((resolve, reject) => {
         reject('실패');
     }
 });
+//다른코드가 들어갈수 있음
+promise
+    .then((message) => {
+        console.log(message); //성공 한 경우 실행
+    })
+    .catch((error) => {
+        console.log(error); // 실패 한 경우 실행
+    })
+    .finally(() => {
+        console.log('무조건'); // 끝나고 무조건 실행
+    });
