58c2f73f7b09036471906050ecc55172d9b05116
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-02-13 22:48:56 +0900

    .

--- es6/promise.js
+++ es6/promise.js
@@ -17,3 +17,22 @@ promise
     .finally(() => {
         console.log('무조건'); // 끝나고 무조건 실행
     });
+
+promise
+    .then((message) => {
+        return new Promise((resolve, reject) => {
+            resolve(message);
+        });
+    })
+    .then((message2) => {
+        console.log(message2);
+        return new Promise((resolve, reject) => {
+            resolve(message2);
+        });
+    })
+    .then((message3) => {
+        console.log(message3);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
