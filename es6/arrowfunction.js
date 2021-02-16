972d9c3594a25d9df9203d7b141ad4a56186c0bb
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-02-16 11:16:05 +0900

    comment out

--- es6/arrowfunction.js
+++ es6/arrowfunction.js
@@ -1,3 +1,8 @@
+/*
+화살표 함수는 function대신 =>기호로 함수를 선언, 변수에 대입하면 재사용 가능.
+함수 내부에 return문밖에 없는경우 중괄호 대신 바로 return할 식을 적어도 됨(ex)add3, add4)
+매개변수가 한개면 소괄호로 묶지 않아도 됨.(ex)not2)
+*/
 function add1(x, y){
     return x + y;
 }
@@ -16,11 +21,16 @@ function not1(x){
 
 const not2 = x => !x;
 
+/*
+기존 function과 다른점은 this 바인드 방식.
+
+*/
+
 var relationship1 = {
     name : 'jiwoong',
     friends : ['jiwoong1', 'jiwoong2', 'jiwoong3'],
     logFriends : function(){
-        var that = this;
+        var that = this; //relationship1을 가르키는 this를 that에 저장
         this.friends.forEach(function (friend){
             console.log(that.name, friend);
         });
--- es6/objectliteral.js
+++ es6/objectliteral.js
@@ -1,7 +1,13 @@
+/*
+객체의 메서드에 함수를 연결할때 콜론(:)과 function을 붙이지 않아도 됨.(ex)sayJS)
+속성명과 변수명이 동일한 경우 한 번만 써도 됨.(ex)sayNode)
+객체의 속성명을 동적으로 생성 할 수 있음. 그 이전에는 객체리터럴 밖에서 해야했음.(ex)[es + 6])
+*/
 var sayNode = function(){
     console.log('Node');
 };
 var es = 'ES';
+
 var oldObject = {
     sayJS : function(){
         console.log('JS');
