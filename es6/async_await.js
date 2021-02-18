9464d3f4071e747210cba1ec4f5633b62aca8ff3
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-02-18 21:49:07 +0900

    .

--- es6/async_await.js
+++ es6/async_await.js
@@ -1,3 +1,10 @@
+/*
+프로미스가 콜백지옥 문제를 해결했지만 then과 catch가 계속 반복되므로 코드가 깊어지진 않지만 여전히 코드가 길어짐.
+async/await로 프로미스를 사용한 코드를 한 번 더 깔끔하게 줄여줄수 있음.
+사용법
+1.일반 함수 선언부(function)대신 async function을 사용, 프로미스 앞에 await를 붙임.
+2.함수는 프로미스가 resolve가 될때까지 기다림.
+*/
 function findAndSaveUser(Users){
     Users.findOne({})
     .then((user) => {
