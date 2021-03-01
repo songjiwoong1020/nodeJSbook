6d88dfdd115dda4982587ee18a5b76d2fa51f9d1
songjiwoong1020 <songjiwoong1020@gmail.com>
2021-03-01 22:54:14 +0900

    .

--- 03/3.3func.js
+++ 03/3.3func.js
@@ -1,7 +1,7 @@
 /*
 불러올 모듈의 경로를 require함수 안에 적는다.(js나 json같은 확장자는 생략 가능.)
 */
-const {odd, even} = require('./var');
+const {odd, even} = require('./3.3var');
 
 function checkOddOrEven(num){
     if(num % 2){
